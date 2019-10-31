518de8076cd4a94d09d531582944df326bd53d02
BreezyP <jorge.a.garcia12@gmail.com>
2019-10-31 13:24:18 -0500

    adding the na tours project

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) =>{
+    res.status(200).json({message: 'Hello from the server side', app: 'This is another message.'});
+});
+
+app.post('/', (req, res) => {
+    res.status(200).send('You can post to this URL');
+});
+
+const port = 3000;
+app.listen(port, () =>{
+    console.log(`App running on port ${port}...`)
+});
