cf8aeb63ae51001e58a6d6f123ee72656ff48c2f
BreezyP <jorge.a.garcia12@gmail.com>
2019-11-05 21:04:07 -0600

    added mongodb atlas and mongoose

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({path: './config.env'});
 const app = require('./app');
 
+
+dotenv.config({path: './config.env'});
+
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+
+//mongoose.connect(process.env.DATABASE_LOCAL, { // To connect to the local database
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+}).then(() => console.log('Database connection successful'));
 
 
 
