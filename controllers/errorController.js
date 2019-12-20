71e2e5fed7a2176af1e9a76da0fa0d99380bc09a
BreezyP <jorge.a.garcia12@gmail.com>
2019-12-20 01:52:43 -0600

    added users schema and jason web tokens to our api

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,11 +1,61 @@
+const AppError = require('../utils/appError');
+
+handleCastErrorDB = (err) => {
+    const message = `Invalid ${err.path}: ${err.value}.`;
+    return new AppError(message, 400);
+};
+
+const sendErrorDev = (err, res) => {
+    res.status(err.statusCode).json({
+        status: err.status,
+        error: err,
+        message: err.message,
+        stack: err.stack
+    });
+};
+
+const sendErrorProd = (err, res) => {
+
+    if(err.isOperational){
+        //Operational trusted error: send message to client
+        res.status(err.statusCode).json({
+            status: err.status,
+            message: err.message
+        });
+    } else {
+        //Programming or other unknown error: don't leak error details
+
+        // 1) Log error
+        console.log('Error', err);
+
+        // 2) Send generic message
+        res.status(500).json({
+            status: 'error',
+            message: 'Something went very wrong!'
+        });
+    }
+};
+
 module.exports = ((err, req, res, next) => {
     console.log(err.stack);
 
     err.statusCode = err.statusCode || 500;
     err.status = err.status || 'error';
 
-    res.status(err.statusCode).json({
-        status: err.status,
-        message: err.message
-    });
+
+    if(process.env.NODE_ENV === 'development'){
+        sendErrorDev(err, res);
+    }
+
+    if(process.env.NODE_ENV === 'production'){
+
+        let error = {...err};
+
+        if(err.name === 'CastError'){
+            error = handleCastErrorDB(error);
+        }
+
+        sendErrorProd(err, res);
+    }
+
 });
---
+++ models/userModel.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+
+// name, email, photo, password, passwordConfirm
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Please tell us your name"],
+    },
+    email: {
+        type: String,
+        required: [true, "a user must have an email"],
+        unique: true,
+        lowercase: true,
+        validate: [validator.isEmail, 'Please provide a valid email']
+    },
+    photo: String,
+    password: {
+        type: String,
+        required: [true, "Please provide a password"],
+        minlength: 8
+    },
+    passwordConfirm: {
+        type: String,
+        required: [true, "Please confirm your password"],
+        validate: {
+            validator: function(cur) {
+                return cur === this.password;
+            },
+            message: "Passwords are not the same"
+        }
+    }
+});
+
+userSchema.pre('save', async function(next){
+
+    //Only run this function if the password was actually modified
+    if(!this.isModified('password')) return next();
+
+    //Hash this password with the cost of 12
+    this.password = await bcrypt.hash(this.password, 12);
+
+    //Delete the passwordConfirm field
+    this.passwordConfirm = undefined;
+    next();
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
