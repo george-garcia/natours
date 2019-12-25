3a53fbf7b612c509a95ef88a7700d926a54b747e
BreezyP <jorge.a.garcia12@gmail.com>
2019-12-25 00:47:31 -0600

    added more features

--- models/userModel.js
+++ models/userModel.js
@@ -41,7 +41,12 @@ const userSchema = new mongoose.Schema({
     },
     passwordChangedAt: Date,
     passwordResetToken: String,
-    passwordResetExpires: Date
+    passwordResetExpires: Date,
+    active: {
+        type: Boolean,
+        default: true,
+        select: false
+    }
 });
 
 userSchema.pre('save', async function(next){
@@ -65,6 +70,13 @@ userSchema.pre('save', function(next){
     //If this gets saved after the token is issued then the user won't be able
     //to actually use their token
     this.passwordChangedAt = Date.now() - 1000;
+    next();
+});
+
+userSchema.pre(/^find/, function(next){
+    // this points to the current query
+    // this is query middleware
+    this.find({ active: {$ne: false} });
     next();
 });
 
