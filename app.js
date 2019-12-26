a44144ec5621df8a61bc1cce1bc97c50bf0b3816
BreezyP <jorge.a.garcia12@gmail.com>
2019-12-25 23:01:37 -0600

    added nosql injection and css protections

--- app.js
+++ app.js
@@ -3,17 +3,49 @@ const fs = require('fs');
 const morgan = require('morgan');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const app = express();
 
-
-//middleware that allows us to take in json from post requests to our server.
-app.use(express.json());
+//GLOBAL MIDDLEWARE
 //middleware that allows with logging
+
+//set security http headers
+app.use(helmet());
 
 if(process.env.NODE_ENV === 'development'){
     app.use(morgan('dev'));
 }
+
+// limit requests
+const limiter = rateLimit({
+    //max 100 requests per hour
+    max: 100,
+    //60 seconds * 60 minutes * 1000 milliseconds or 1 hour in milliseconds
+    windowMS: 60 * 60 * 1000,
+    message: 'Too many requests from this IP, please try again in an hour!'
+});
+
+app.use('/api', limiter);
+
+//middleware that allows us to take in json from post requests to our server.
+// Body parse, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+// Data sanitization against NoSQL query injection
+
+// Data sanitization against XSS
+
+// Serving static files
+app.use(express.static(`${__dirname}/public`));
+
+// Test middleware
+app.use((req, res, next) => {
+    req.requestTime = new Date().toISOString();
+    // console.log(req.headers);
+    next();
+});
 
 app.use((req, res, next) => {
     console.log('Hello from the middleware');
--- controllers/authController.js
+++ controllers/authController.js
@@ -5,7 +5,6 @@ const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 const sendEmail = require('../utils/email');
 const crypto = require('crypto');
-const bcrypt = require('bcryptjs');
 
 const signToken = id => {
     return jwt.sign({ id: id }, process.env.JWT_SECRET, {
@@ -15,6 +14,18 @@ const signToken = id => {
 
 const createSendToken = (user, statusCode, res) => {
     const token = signToken(user._id);
+    const cookieOptions = {
+        expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
+        secure: true,
+        httpOnly: true,
+    };
+
+    if(process.env.NODE_ENV ==='production') cookieOptions.secure = true;
+
+    res.cookie('jwt', token, cookieOptions);
+
+    // Remove password from output
+    user.password = undefined;
 
     res.status(statusCode).json({
         status: 'success',
@@ -146,6 +157,7 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
         return next(new AppError('There was an error sending the email. Try again later!', 500));
     }
 });
+
 exports.resetPassword = catchAsync( async (req, res, next) => {
     // 1) Get user based on the token
     const hashedToken = crypto.createHash('sha256').update(req.params.token).digest('hex');
